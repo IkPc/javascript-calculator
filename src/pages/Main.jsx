d86273a4d81f7467a3be6c3930e9e5ffc10ac788
Pabllo Cristian <pabllocristian10lima@hotmail.com>
2024-01-28 22:24:43 -0300

    display fix

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Main.scss";
 import Buttons from "../components/Buttons";
-{/*import Display from "../components/Display";*/}
+import Display from "../components/Display";
 
 const Main = () => (
     <div>
         <div className="header">
         </div>
         <div className="content">
-            {/*<Display />*/}
+            <Display />
             <Buttons />
         </div>
         <div className="footer">
